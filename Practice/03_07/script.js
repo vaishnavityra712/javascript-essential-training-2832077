ffb982702f4f31184597b2f09068561575c5d8b7
VAISHNAVI ACHANTA <vaishnavityra712@gmail.com>
2021-08-18 20:30:03 +0530

    practice files

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,36 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+Teddy = {
+    made: "Cloth",
+    stuffed: "cotton",
+    eyes: {
+        right: 1,
+        left: 1,
+    },
+    hands: 3,
+    legs: 2,
+    nose: 1,
+}
+console.log(Teddy);
+
+Dogs = {
+    type: "animals",
+    legs: {
+        front: 2,
+        back: 2,
+    },
+    favourite_thing: "Cuddling",
+    Food: ["dog food", "Chicken", "Bones"],
+    play: "zoomies",
+}
+console.log(Dogs);
+
+Office = {
+    desk: 24,
+    chairs: 48,
+    pc: 24,
+    assined_to: "Employee",
+    flower: "roses",
+}
