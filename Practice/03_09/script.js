ae9ea5c48cbc668e68db771fbb8459f8c63f0054
VAISHNAVI ACHANTA <vaishnavityra712@gmail.com>
2021-08-19 16:09:18 +0530

    functions created

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -8,19 +8,30 @@
  */
 
 const backpack = {
-  name: "Everyday Backpack",
-  volume: 30,
-  color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
-  },
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
-  },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-  },
+    name: "Everyday Backpack",
+    volume: 30,
+    color: "grey",
+    pocketNum: 15,
+    strapLength: {
+        left: 26,
+        right: 26,
+    },
+    toggleLid: function(lidStatus) {
+        this.lidOpen = lidStatus;
+    },
+    volume_change: function(vol) {
+        this.volume = vol;
+    },
+    newStrapLength: function(lengthLeft, lengthRight) {
+        this.strapLength.left = lengthLeft;
+        this.strapLength.right = lengthRight;
+    },
+    color_backpack: function(col) {
+        this.color = col
+    }
 };
+backpack.volume_change(50)
+console.log("The new volume change :", backpack.volume)
+
+backpack.color_backpack("Pink")
+console.log("New color: ", backpack.color)
