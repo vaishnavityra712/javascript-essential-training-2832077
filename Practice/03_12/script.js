f2b333bc7fec8512eedf25ee1a70dd1a21d08b51
VAISHNAVI ACHANTA <vaishnavityra712@gmail.com>
2021-08-19 17:08:46 +0530

    made constructors

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,40 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+import Teddy from "./Teddy.js";
+
+const softtoy = new Teddy(
+    "cloths",
+    "cotton",
+    2,
+    2,
+    3,
+    4,
+    1,
+);
+console.log("Softtoy object: ", softtoy);
+
+import Backpack from './Backpack.js';
+
+const everydaypack = new Backpack(
+    "puma",
+    45,
+    "red",
+    15,
+    25,
+    25,
+    true
+)
+console.log("everydaypack object: ", everydaypack);
+
+import Office from "./Office.js";
+
+const work = new Office(
+    24,
+    48,
+    24,
+    48,
+    48,
+)
+console.log("work object: ", work);
