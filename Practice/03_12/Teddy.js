236a6d669149ee8d6b27488922aea8c9dda9a5f1
VAISHNAVI ACHANTA <vaishnavityra712@gmail.com>
2021-09-12 13:20:09 +0530

    exercise completes

---
+++ Practice/03_12/Teddy.js
@@ -0,0 +1,24 @@
+class Teddy {
+    constructor(
+        //define parameters
+        made,
+        stuffed,
+        right,
+        left,
+        hands,
+        legs,
+        nose,
+    ) {
+        //define prop
+        this.made = made;
+        this.stuffed = stuffed;
+        this.left = left;
+        this.right = right;
+        this.hands = hands;
+        this.legs = legs;
+        this.nose = nose;
+
+    }
+}
+
+export default Teddy;
--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,20 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+var x = document.querySelectorAll("h2.page-header__heading");
+x[0].style.backgroundColor = "pink";
+
+var y = document.querySelectorAll("p");
+y[0].style.backgroundColor = "#ac80ed"
+y[0].style.opacity = "0.5"
+
+var z = document.querySelectorAll("main article li:last-child");
+z.forEach(item => item.style.backgroundColor = "blue");
+/*
+forEach(item => item.style.background = "colorname") ==
+var i;
+for(i = 0; i < z.length; i++){
+    z[i].style.backgroundColor = "colorname";
+}
+
+*/
